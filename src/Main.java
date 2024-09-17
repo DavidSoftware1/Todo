997cfec948d4f9a0999844e179067b3ac2e0bdbd
Student <davidhoffert@ymail.com>
2024-09-17 09:03:03 +0200

    git comit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //usereingabe
+        //Controller mit midt den cases basteln
+        //TOdo Klasse erzeugung
+        System.out.println("Start...");
+
+        //TODO Lorem ipsum dolor sit amet
+
+
+        System.out.println("Ende....");
+    }
+}
